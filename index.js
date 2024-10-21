8ed12e90b629ca6aa49141746d632d427d27664f
DAVI HEINZEN DE SOUZA <130378822+Davisoukkj@users.noreply.github.com>
2024-10-21 14:34:25 +0000

    criado um midware para pasta public

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 
 const PORT = 3343;
+//CRIAÇÃO DE ROTAS
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.send("HOJE TEM FUUUUUUUTEEEEEBAAAAS");
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+//CRIAÇÃO ROTA 404 (MIDDLEWARE)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
